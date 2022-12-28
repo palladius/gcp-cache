e945314c32bbf3d8a5ce3d7ffe285243555d3b0e
Riccardo Carlesso <palladiusbonton@gmail.com>
2022-12-28 11:38:45 +0000

    great visualization of assets now :)

--- app/models/inventory_item.rb
+++ app/models/inventory_item.rb
@@ -1,8 +1,6 @@
 class InventoryItem < ApplicationRecord
     serialize :serialized_ancestors
 
-
-
     def ancestors
         serialized_ancestors rescue []
     end
@@ -10,8 +8,18 @@ class InventoryItem < ApplicationRecord
     def simplified_type
         asset_type.split("/").last
     end
+    def simplified_name
+        name.split("/").last
+    end
+
+    def organization_id
+        ancestors.select{|x| x=~ /^organizations/ }[0].split('/').second rescue nil
+    end
+    def organization
+        Folder.find_by_folder_id(organization_id) # rescue nil
+    end
 
     def to_s
-        "#{simplified_type} :#{INVENTORY_ITEM_ICON} : #{name}"
+        "#{simplified_type} :#{INVENTORY_ITEM_ICON} : #{simplified_name}"
     end
 end
