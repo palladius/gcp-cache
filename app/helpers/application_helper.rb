5a368a81cc64eb4cc2928610b20db0692f20217c
Riccardo Carlesso (Derek) <palladiusbonton@gmail.com>
2023-04-14 11:22:54 +0200

    awesome DB emojified

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,4 +12,21 @@ module ApplicationHelper
         # if model has no active -> error
         model.active ? '🚴' : '🩶'  rescue '?!?'
     end
+
+    def render_db_with_emoji()
+        config = ActiveRecord::Base.configurations.configs_for(env_name: Rails.env ).first
+        adapter = config.adapter
+        db = config.database
+        emoji = case adapter
+        when 'postgresql'
+            '🐘pg🛢'
+        when 'mysql'
+            '🐬my🛢'
+        when 'sqlite', 'sqlite3'
+            '🪶lt🛢'
+        else 
+            '🛢?!?🛢'
+        end
+        "#{emoji} <b>#{db}</b>".html_safe
+    end
 end
